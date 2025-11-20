705ce17af43c9ca4c17c89804b7b427e50edc8bf
carlotadelriego <carlota.fernandez.01@uie.edu>
2025-11-20 11:07:12 +0100

    Creados los archivos en los que se aplican los modelos de generar tts y bark

---
+++ src/generar_bark.py
@@ -0,0 +1,21 @@
+from bark import SAMPLE_RATE, generate_audio, preload_models
+from scipy.io.wavfile import write
+import numpy as np
+
+# Pre-carga de modelos
+preload_models()
+
+texto = "Este es un ejemplo generado con el modelo Bark para la práctica."
+audio_referencia = "audio/audio_original.wav"
+
+# Generar audio con zero-shot
+audio = generate_audio(
+    texto,
+    history_prompt=audio_referencia
+)
+
+# Guardar audio
+audio_np = np.array(audio)
+write("audio/output_bark.wav", SAMPLE_RATE, audio_np)
+
+print("Audio generado y guardado como audio/output_bark.wav")
